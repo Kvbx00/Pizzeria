58485b747bba0598d5bfb731cf34087cb28c9dd7
kubap <kubapawlowski98@gmail.com>
2022-12-29 12:45:10 +0100

    zxc

--- src/main/java/com/pizzeria/controller/ProductController.java
+++ src/main/java/com/pizzeria/controller/ProductController.java
@@ -1,21 +1,28 @@
 package com.pizzeria.controller;
 
+import com.pizzeria.model.Product;
 import com.pizzeria.repository.ProductRepository;
-import com.pizzeria.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+
     @Autowired
-    private ProductRepository pRepo;
+    private ProductRepository productRepository;
+
     @GetMapping({"/menu"})
-    public ModelAndView getAllProducts(){
-        ModelAndView mav = new ModelAndView("menu");
-        mav.addObject("product", pRepo.findAll());
-        return mav;
+    public String productByPizza(Model model) {
+        List<Product> product_appetizer = productRepository.findProductByProduct_category("przystawki");
+        List<Product> product_pizza = productRepository.findProductByProduct_category("pizza");
+        List<Product> product_pasta = productRepository.findProductByProduct_category("pasta");
+        model.addAttribute("product_appetizer", product_appetizer);
+        model.addAttribute("product_pizza", product_pizza);
+        model.addAttribute("product_pasta", product_pasta);
+        return "menu";
     }
 }
--- src/main/java/com/pizzeria/repository/ProductRepository.java
+++ src/main/java/com/pizzeria/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package com.pizzeria.repository;
 
 import com.pizzeria.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    @Query(value = "SELECT * FROM product WHERE product_category = ?1", nativeQuery = true)
+    List<Product>findProductByProduct_category(String product_category);
 }
