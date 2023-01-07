4490a0ea5578c8740aad9bd742fe1f8900bffa78
Jakub <kubapawlowski98@gmail.com>
2023-01-07 18:07:51 +0100

    zxc

--- src/main/java/com/pizzeriaRestaurant/model/Customer.java
+++ src/main/java/com/pizzeriaRestaurant/model/Customer.java
@@ -5,48 +5,53 @@ import javax.persistence.Id;
 
 @Entity
 public class Customer {
-	@Id
-	private String email;
-	private String name;
-	private String password;
-	private long contact;
-	
-	public Customer() {
-		super();
-	}
-	
-	public Customer(String email, String name, String password, long contact) {
-		super();
-		this.email = email;
-		this.name = name;
-		this.password = password;
-		this.contact = contact;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public long getContact() {
-		return contact;
-	}
-	public void setContact(long contact) {
-		this.contact = contact;
-	}
-	
-	
+    @Id
+    private String email;
+    private String name;
+    private String password;
+    private Integer contact;
+
+    public Customer() {
+        super();
+    }
+
+    public Customer(String email, String name, String password, Integer contact) {
+        super();
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getContact() {
+        return contact;
+    }
+
+    public void setContact(Integer contact) {
+        this.contact = contact;
+    }
 }
