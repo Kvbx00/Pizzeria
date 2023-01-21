ab9ebd6dca87c691929ffcd9cc9adea6cb2d9ec6
Jakub <kubapawlowski98@gmail.com>
2023-01-21 11:58:51 +0100

    zxc

--- src/main/java/com/pizzeria/controller/OrdersController.java
+++ src/main/java/com/pizzeria/controller/OrdersController.java
@@ -8,13 +8,20 @@ import com.pizzeria.service.CustomerService;
 import com.pizzeria.service.OrderDetailsService;
 import com.pizzeria.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Null;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
@@ -54,10 +61,40 @@ public class OrdersController {
         return "/manageOrders";
     }
 
+
     @GetMapping("/searchOrders")
     public String searchOrders(@RequestParam("name") String name, Model model) {
         List<Orders> orders = ordersRepository.findByOrderDetailsName(name);
-        model.addAttribute("orders", orders);
-        return "/searchOrders";
+        if (orders.isEmpty()){
+            model.addAttribute("action", "Brak zamówień dla podanego produktu");
+            model.addAttribute("orders", ordersService.getAllOrders());
+            return "manageOrders";
+        }else{
+            model.addAttribute("orders", orders);
+            return "/searchOrders";
+        }
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
+    }
+    @GetMapping("/searchOrdersDate")
+    public String searchOrders(Model model, @RequestParam(value = "date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        if(date != null) {
+            List<Orders> orders = ordersRepository.findByDate(date);
+            if (orders.isEmpty()) {
+                model.addAttribute("action", "Brak zamówień w wybranym dniu");
+                model.addAttribute("orders", ordersService.getAllOrders());
+                return "manageOrders";
+            } else {
+                model.addAttribute("orders", orders);
+                return "/searchOrders";
+            }
+        } else {
+            model.addAttribute("action", "Nie wpisano daty!");
+            model.addAttribute("orders", ordersService.getAllOrders());
+            return "manageOrders";
+        }
     }
 }
--- src/main/java/com/pizzeria/repository/OrdersRepository.java
+++ src/main/java/com/pizzeria/repository/OrdersRepository.java
@@ -5,11 +5,14 @@ import com.pizzeria.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
     List<Orders> findByCustomer(Customer customer);
     List<Orders> findByOrderDetailsName(String name);
+
+    List<Orders> findByDate(Date date);
 
 }
