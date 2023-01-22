407759f48e49c0ee5b3bfa1ab335022f07a6abf6
Jakub <kubapawlowski98@gmail.com>
2023-01-22 12:31:04 +0100

    zxc

--- pizzeria.sql
+++ pizzeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 14 Sty 2023, 20:13
+-- Czas generowania: 22 Sty 2023, 12:29
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -79,16 +79,50 @@ INSERT INTO `customer` (`id`, `email`, `contact`, `name`, `password`, `address`)
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `id` int(11) NOT NULL,
+  `productid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `totalcost` float NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `orderdetails`
+--
+
+INSERT INTO `orderdetails` (`id`, `productid`, `name`, `quantity`, `totalcost`, `order_id`) VALUES
+(21, 17497, 'NAPOLETANA', 2, 48, 439619),
+(22, 60265, 'HAWAJSKA', 1, 29, 439619),
+(23, 17497, 'NAPOLETANA', 1, 24, 541785),
+(24, 76528, 'MARGHERITA', 2, 44, 541785),
+(25, 79261, 'CAPRICCIOSA', 3, 96, 541785),
+(27, 79261, 'CAPRICCIOSA', 1, 32, 407508),
+(28, 76528, 'MARGHERITA', 3, 66, 407508),
+(29, 77801, 'GRECO', 2, 60, 101993),
+(30, 82833, 'SALAMELLA', 1, 29, 101993),
+(31, 17497, 'NAPOLETANA', 1, 24, 669382),
+(32, 28604, 'SALAME DOLCE', 1, 28, 669382),
+(33, 90459, 'MESSICANA', 2, 64, 543320),
+(34, 89862, 'MARINARA', 1, 23, 543320),
+(35, 17497, 'NAPOLETANA', 2, 48, 516712),
+(36, 79261, 'CAPRICCIOSA', 3, 96, 516712),
+(37, 76528, 'MARGHERITA', 3, 66, 516712);
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `orders`
 --
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `productid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `totalcost` float NOT NULL,
+  `price` float NOT NULL,
   `customer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
@@ -96,16 +130,14 @@ CREATE TABLE `orders` (
 -- Zrzut danych tabeli `orders`
 --
 
-INSERT INTO `orders` (`id`, `date`, `productid`, `name`, `quantity`, `totalcost`, `customer_id`) VALUES
-(129787, '2023-01-14', 28604, 'SALAME DOLCE', 1, 28, 1),
-(267869, '2023-01-14', 77801, 'GRECO', 1, 30, 3),
-(306259, '2023-01-14', 76528, 'MARGHERITA', 2, 44, 1),
-(379754, '2023-01-14', 89862, 'MARINARA', 2, 44, 1),
-(391705, '2023-01-14', 79261, 'CAPRICCIOSA', 1, 32, 1),
-(457533, '2023-01-14', 17497, 'NAPOLETANA', 1, 24, 1),
-(490587, '2023-01-14', 28604, 'SALAME DOLCE', 2, 56, 3),
-(556499, '2023-01-14', 90459, 'MESSICANA', 1, 32, 1),
-(822993, '2023-01-14', 77801, 'GRECO', 3, 90, 1);
+INSERT INTO `orders` (`id`, `date`, `price`, `customer_id`) VALUES
+(101993, '2023-01-22', 89, 3),
+(407508, '2023-01-22', 98, 1),
+(439619, '2023-01-22', 77, 1),
+(516712, '2023-01-22', 210, 3),
+(541785, '2023-01-22', 164, 1),
+(543320, '2023-01-22', 87, 3),
+(669382, '2023-01-22', 52, 3);
 
 -- --------------------------------------------------------
 
@@ -159,6 +191,13 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
 -- Indeksy dla tabeli `orders`
 --
 ALTER TABLE `orders`
@@ -182,10 +221,16 @@ ALTER TABLE `customer`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT dla tabeli `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
 -- AUTO_INCREMENT dla tabeli `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=822994;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=992834;
 
 --
 -- AUTO_INCREMENT dla tabeli `product`
@@ -196,6 +241,12 @@ ALTER TABLE `product`
 --
 -- Ograniczenia dla zrzutów tabel
 --
+
+--
+-- Ograniczenia dla tabeli `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
 -- Ograniczenia dla tabeli `orders`
